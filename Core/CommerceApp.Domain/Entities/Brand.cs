b928c1cf9e56a9cd2a0500dfc40e828101e7494c
Melih Şimşek <Melihsimsek02@gmail.com>
2024-11-16 16:48:58 +0300

    Base entities created

---
+++ Core/CommerceApp.Domain/Entities/Brand.cs
@@ -0,0 +1,26 @@
+﻿using CommerceApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommerceApp.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        public required string Name { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+
+        public Brand()
+        {
+        }
+
+        public Brand(string name)
+        {
+            Name = name;
+        }
+
+
+    }
+}
---
+++ Core/CommerceApp.Domain/Entities/Category.cs
@@ -0,0 +1,29 @@
+﻿using CommerceApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommerceApp.Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priority { get; set; }
+        public virtual ICollection<Detail> Details { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+
+        public Category()
+        {
+        }
+
+        public Category(int parentId, string name, int priority)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priority = priority;
+        }
+    }
+}
